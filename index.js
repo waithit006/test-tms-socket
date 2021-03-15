bc489cb03267f363bc0093c535146b7c2fd57ee2
Waithid <waithit006@gmail.com>
2021-03-16 00:51:48 +0700

    add emit load

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ http.listen(4001, () => {
 });
 
 io.on('connect', socket => {
+    console.log("Connected");
     socket.on('updateLatLng', (message) => {
-        console.log(message);
+        socket.emit('loadLatLng',{msg:message,ip:socket.handshake.address})
     })
+    
 });
